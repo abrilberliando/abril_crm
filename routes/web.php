755d74f865bd774a3c8e2b906b528fc44496ef90
abrilberliando <abrilbrlnd2@gmail.com>
2025-08-02 14:31:05 +0700

    fix: deployment

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Filament::routes();  // Pastikan Filament routes ada di sini
-});
