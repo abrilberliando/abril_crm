d561a689f9c08535c8dea7f00add8fb7a75b6ed9
abrilberliando <abrilbrlnd2@gmail.com>
2025-08-02 14:36:13 +0700

    fix: dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,9 +3,6 @@
 namespace App\Filament\Pages;
 
 use App\Filament\Widgets\ManagerStatsOverview;
-use App\Filament\Widgets\MyRecentLeadsTable;
-use App\Filament\Widgets\NewCustomersChart;
-use App\Filament\Widgets\SalesStatsOverview;
 use Filament\Pages\Dashboard as BasePage;
 
 class Dashboard extends BasePage
@@ -18,18 +15,8 @@ class Dashboard extends BasePage
         if ($user->hasRole('manager')) {
             return [
                 ManagerStatsOverview::class,
-                NewCustomersChart::class,
             ];
         }
-
-        // Jika user memiliki role 'sales'
-        if ($user->hasRole('sales')) {
-            return [
-                SalesStatsOverview::class,
-                MyRecentLeadsTable::class,
-            ];
-        }
-
         // Dashboard kosong untuk role lain
         return [];
     }
